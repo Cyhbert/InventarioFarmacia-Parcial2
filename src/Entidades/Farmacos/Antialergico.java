79b9ed51a1b312b79c619e58a4eb187ceab156cf
Galdámez <al.galdamez@protonmail.com>
2020-04-23 12:08:46 -0600

    Clase Antialergico agregada.

---
+++ src/Entidades/Farmacos/Antialergico.java
@@ -0,0 +1,24 @@
+package Entidades.Farmacos;
+
+public class Antialergico extends Farmaco {
+
+     String tipoProducto;
+
+     public Antialergico(String idProducto, String nombreProducto, String tipoVenta, double precio, int cantDisponible) {
+          super.setIdProducto(idProducto);
+          super.setNombre(nombreProducto);
+          super.setTipoVenta(tipoVenta);
+          super.setPrecio(precio);
+          super.setCantDisponible(cantDisponible);
+          this.tipoProducto = "Antiacido";
+     }
+
+     public String getTipoProducto() {
+          return tipoProducto;
+     }
+
+     public void setTipoProducto(String tipoProducto) {
+          this.tipoProducto = tipoProducto;
+     }
+
+}
